71372fc33262291cd1f44ac1502051324eab6419
sazonov.den <devnull@localhost>
2019-03-12 22:17:08 +0600

    команды ov - мелочи (убраны лишние параметры)

--- jdtx-repl-main/test/jdtx/repl/main/ModuleTest.java
+++ jdtx-repl-main/test/jdtx/repl/main/ModuleTest.java
@@ -1,16 +1,26 @@
 package jdtx.repl.main;
 
-import jandcode.app.test.*;
-import org.junit.*;
+import jandcode.app.test.AppTestCase;
+import jandcode.bgtasks.BgTasksService;
+import org.junit.Test;
 
 public class ModuleTest extends AppTestCase {
 
     {
-        logSetUp = true;
+        logSetUp = false;
     }
 
     @Test
     public void test1() throws Exception {
         app.saveAppRt();
     }
+
+    @Test
+    public void test_bg() throws Exception {
+        BgTasksService bgTasksService = app.service(BgTasksService.class);
+        String cfgFileName = bgTasksService.getRt().getChild("bgtask").getChild("ws").getValueString("cfgFileName");
+        System.out.println("cfgFileName: " + cfgFileName);
+    }
+
+
 }
