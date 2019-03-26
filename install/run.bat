1c2f36f5781e9a939b4ab68a3b8d0b3dc2abac49
sazonov.den <devnull@localhost>
2019-03-26 13:17:50 +0600

    -

--- install/run.bat
+++ install/run.bat
@@ -6,7 +6,7 @@ cd /d %CURR_DIR%
 
 rem Готовим файл запуска в скрытом режиме
 echo Set WshShell = CreateObject("WScript.Shell") > start.vbs
-echo WshShell.Run "cmd.exe /c run-jc.bat", 1, false >> start.vbs
+echo WshShell.Run "cmd.exe /c run-jc.bat", 0, false >> start.vbs
  
 rem Запускает на выполнение в скрытом режиме, конца выполнения не ждём.
 cscript.exe //b //Logo start.vbs
