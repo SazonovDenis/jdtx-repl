6cd71516582d8e26dee6a3dc70f046a5520a5363
sazonov.den <sazonov.den@gmail.com>
2021-04-23 14:27:41 +0600

    Образец cfg

--- install/cfg/set-cfg.bat
+++ install/cfg/set-cfg.bat
@@ -12,6 +12,7 @@ rem ƒл€ сервера напр€мую задаем структуру публикаций
 call jc repl_set_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json
 call jc repl_set_cfg -cfg:cfg_publications -file:%cfg_path%publication_lic_194_srv.json
 
+
 rem –ассылаем на рабочие станции новую стратегию перекодировки
 call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -ws:1
 call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -ws:2
@@ -26,6 +27,7 @@ call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -
 call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -ws:11
 call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -ws:12
 call jc repl_send_cfg -cfg:cfg_decode -file:%cfg_path%decode_strategy_194.json -ws:13
+
 
 rem –ассылаем на рабочие станции новые правила публикаций
 call jc repl_send_cfg -cfg:cfg_publications -file:%cfg_path%publication_lic_194_srv.json -ws:1
@@ -42,10 +44,27 @@ call jc repl_send_cfg -cfg:cfg_publications -file:%cfg_path%publication_lic_194_
 call jc repl_send_cfg -cfg:cfg_publications -file:%cfg_path%publication_lic_194_ws.json -ws:12
 call jc repl_send_cfg -cfg:cfg_publications -file:%cfg_path%publication_lic_194_ws.json -ws:13
 
+
 rem «авершение изменени€ структуры
 pause Wait finish
 call jc repl-dbstruct-finish
-pause Done
+
+pause Done finish
 
 
-ov repl-request-snapshot -ws:all -table:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel" + что то от EsilMk
+rem «апрос snapshot
+call jc repl-request-snapshot -ws:1  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:2  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:3  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:4  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:5  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:6  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:7  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:8  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:9  -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:10 -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:11 -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:12 -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+call jc repl-request-snapshot -ws:13 -tables:"CommentText,CommentTip,Usr,UsrGrp,UsrOtdel"
+
+pause Done snapshot
