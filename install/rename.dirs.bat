c4119ad6897d856bf65750b4cd2070d0cc05943c
sazonov.den <sazonov.den@gmail.com>
2021-05-12 15:36:44 +0600

    Для обновления

--- install/rename.dirs.bat
+++ install/rename.dirs.bat
@@ -7,54 +7,77 @@ rename web\WEB-INF\data\srv\queCommon    que_Common
 rename web\WEB-INF\data\ws_001\queIn     que_in   
 rename web\WEB-INF\data\ws_001\queIn001  que_in001
 rename web\WEB-INF\data\ws_001\queOut    que_out  
+mkdir  web\WEB-INF\data\ws_001\que_in001
 
 rename web\WEB-INF\data\ws_002\queIn     que_in
 rename web\WEB-INF\data\ws_002\queIn001  que_in001
 rename web\WEB-INF\data\ws_002\queOut    que_out
+mkdir  web\WEB-INF\data\ws_002\que_in001
 
 rename web\WEB-INF\data\ws_003\queIn     que_in
 rename web\WEB-INF\data\ws_003\queIn001  que_in001
 rename web\WEB-INF\data\ws_003\queOut    que_out
+mkdir  web\WEB-INF\data\ws_003\que_in001
 
 rename web\WEB-INF\data\ws_004\queIn     que_in
 rename web\WEB-INF\data\ws_004\queIn001  que_in001
 rename web\WEB-INF\data\ws_004\queOut    que_out
+mkdir  web\WEB-INF\data\ws_004\que_in001
 
 rename web\WEB-INF\data\ws_005\queIn     que_in
 rename web\WEB-INF\data\ws_005\queIn001  que_in001
 rename web\WEB-INF\data\ws_005\queOut    que_out
+mkdir  web\WEB-INF\data\ws_005\que_in001
 
 rename web\WEB-INF\data\ws_006\queIn     que_in
 rename web\WEB-INF\data\ws_006\queIn001  que_in001
 rename web\WEB-INF\data\ws_006\queOut    que_out
+mkdir  web\WEB-INF\data\ws_006\que_in001
 
 rename web\WEB-INF\data\ws_007\queIn     que_in
 rename web\WEB-INF\data\ws_007\queIn001  que_in001
 rename web\WEB-INF\data\ws_007\queOut    que_out
+mkdir  web\WEB-INF\data\ws_007\que_in001
 
 rename web\WEB-INF\data\ws_008\queIn     que_in
 rename web\WEB-INF\data\ws_008\queIn001  que_in001
 rename web\WEB-INF\data\ws_008\queOut    que_out
+mkdir  web\WEB-INF\data\ws_008\que_in001
 
 rename web\WEB-INF\data\ws_009\queIn     que_in
 rename web\WEB-INF\data\ws_009\queIn001  que_in001
 rename web\WEB-INF\data\ws_009\queOut    que_out
+mkdir  web\WEB-INF\data\ws_009\que_in001
 
 rename web\WEB-INF\data\ws_010\queIn     que_in
 rename web\WEB-INF\data\ws_010\queIn001  que_in001
 rename web\WEB-INF\data\ws_010\queOut    que_out
+mkdir  web\WEB-INF\data\ws_010\que_in001
 
 rename web\WEB-INF\data\ws_011\queIn     que_in
 rename web\WEB-INF\data\ws_011\queIn001  que_in001
 rename web\WEB-INF\data\ws_011\queOut    que_out
+mkdir  web\WEB-INF\data\ws_011\que_in001
 
 rename web\WEB-INF\data\ws_012\queIn     que_in
 rename web\WEB-INF\data\ws_012\queIn001  que_in001
 rename web\WEB-INF\data\ws_012\queOut    que_out
+mkdir  web\WEB-INF\data\ws_012\que_in001
 
 rename web\WEB-INF\data\ws_013\queIn     que_in
 rename web\WEB-INF\data\ws_013\queIn001  que_in001
 rename web\WEB-INF\data\ws_013\queOut    que_out
+mkdir  web\WEB-INF\data\ws_013\que_in001
+
+rename web\WEB-INF\data\ws_014\queIn     que_in
+rename web\WEB-INF\data\ws_014\queIn001  que_in001
+rename web\WEB-INF\data\ws_014\queOut    que_out
+mkdir  web\WEB-INF\data\ws_014\que_in001
+
+rename web\WEB-INF\data\ws_015\queIn     que_in
+rename web\WEB-INF\data\ws_015\queIn001  que_in001
+rename web\WEB-INF\data\ws_015\queOut    que_out
+mkdir  web\WEB-INF\data\ws_015\que_in001
 
 
 rem pause
