5507c1a0a69469685be0fe07eae930481b274fe8
sazonov.den <sazonov.den@gmail.com>
2021-05-30 15:08:16 +0600

    -

---
+++ install/cfg/set-cfg-ws.bat
@@ -0,0 +1,13 @@
+rem @echo off
+
+rem set cfg_path=%~dp0
+set cfg_path=C:\Users\Public\Documents\Jadatex.Sync\web\WEB-INF\cfg\
+
+rem cd ..
+
+@echo -------------------------
+@echo %cfg_path%
+@echo -------------------------
+
+rem Напрямую задаем структуру публикаций
+call jc repl_set_cfg -type:cfg_publications -file:%cfg_path%publication_lic_194_ws.json
