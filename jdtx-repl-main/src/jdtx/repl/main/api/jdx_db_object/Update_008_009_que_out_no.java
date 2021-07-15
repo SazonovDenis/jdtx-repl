52d776dfb4b46df56157f994c8cd85022248b06e
sazonov.den <sazonov.den@gmail.com>
2021-07-15 21:04:56 +0600

    Уточнение по нумерацции queOut при смене структуры

--- jdtx-repl-main/src/jdtx/repl/main/api/jdx_db_object/Update_008_009_que_out_no.java
+++ jdtx-repl-main/src/jdtx/repl/main/api/jdx_db_object/Update_008_009_que_out_no.java
@@ -1,6 +1,7 @@
 package jdtx.repl.main.api.jdx_db_object;
 
 import jandcode.dbm.db.*;
+import jdtx.repl.main.api.*;
 import jdtx.repl.main.api.que.*;
 import org.apache.commons.logging.*;
 
@@ -12,9 +13,12 @@ public class Update_008_009_que_out_no implements ISqlScriptExecutor {
     public void exec(Db db) throws Exception {
         JdxQuePersonal que = new JdxQuePersonal(db, UtQue.QUE_OUT, -1);
         //
-        long age = que.getMaxAge();
+        long que_out_age = que.getMaxAge();
+        long que_out_no = db.loadSql("select max(id) id from " + UtJdx.SYS_TABLE_PREFIX + "que_" + UtQue.getTableSuffix(UtQue.QUE_OUT)).getCurRec().getValueLong("id");
         //
-        que.setMaxNo(age);
+        que.setMaxNo(que_out_no);
+        //
+        log.info("que_out_age: " + que_out_age + ", que_out_no: " + que_out_no);
     }
 
 }
