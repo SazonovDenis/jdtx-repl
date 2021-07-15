83b92f82ee397cea4a6622da6505ef4471950de4
sazonov.den <sazonov.den@gmail.com>
2021-07-15 21:24:16 +0600

    -

--- jdtx-repl-main/src/jdtx/repl/main/api/jdx_db_object/Update_008_009_que_out_no.java
+++ jdtx-repl-main/src/jdtx/repl/main/api/jdx_db_object/Update_008_009_que_out_no.java
@@ -15,7 +15,8 @@ public class Update_008_009_que_out_no implements ISqlScriptExecutor {
         //
         long que_out_age = que.getMaxAge();
         long que_out_no = db.loadSql("select max(id) id from " + UtJdx.SYS_TABLE_PREFIX + "que_" + UtQue.getTableSuffix(UtQue.QUE_OUT)).getCurRec().getValueLong("id");
-        //
+        // Берем именно que_out_no, а не que_out_age (как может показаться правильным).
+        // На некоторых базах нумерация id - сдвинута, из-за чего - не разбирался.
         que.setMaxNo(que_out_no);
         //
         log.info("que_out_age: " + que_out_age + ", que_out_no: " + que_out_no);
