63d76a970a8f77630d46734924ae568f03cd8b7f
sazonov.den <sazonov.den@gmail.com>
2021-11-24 19:46:51 +0600

    -

--- jdtx-repl-main/src/jdtx/repl/main/api/util/UtMail.java
+++ jdtx-repl-main/src/jdtx/repl/main/api/util/UtMail.java
@@ -5,7 +5,6 @@ import jdtx.repl.main.api.mailer.*;
 import jdtx.repl.main.api.manager.*;
 import jdtx.repl.main.api.que.*;
 import jdtx.repl.main.api.replica.*;
-import jdtx.repl.main.api.util.*;
 import org.apache.commons.logging.*;
 
 public class UtMail {
@@ -21,13 +20,57 @@ public class UtMail {
     public static void sendQueToMail(long wsId, IJdxQue que, IMailer mailer, String box, IJdxMailStateManager mailStateManager) throws Exception {
         log.info("sendMail, wsId: " + wsId + ", box: " + box);
 
+        // Выясняем объем передачи
+        UtMailSendTask sendTask = getSendTask(que, mailer, box, mailStateManager);
+
+        // Передаем
+        long count = 0;
+        for (long no = sendTask.sendFrom; no <= sendTask.sendTo; no++) {
+            IReplica replica = que.get(no);
+
+            // Читаем заголовок реплики (чтобы replica.info полностью заполнился - это важно при отправке)
+            JdxReplicaReaderXml.readReplicaInfo(replica);
+
+            // Физически отправим реплику
+            mailer.send(replica, box, no);
+
+            // Отметим отправку очередного номера реплики (если отметка двигается вперед)
+            if (no > sendTask.mailLastSendNo) {
+                mailStateManager.setMailSendDone(no);
+            }
+
+            //
+            count = count + 1;
+        }
+
+        // Снимем флаг просьбы станции
+        if (sendTask.required) {
+            mailer.setSendRequired(box, new SendRequiredInfo());
+            log.warn("Repeat send done");
+        }
+
+        // Отметим попытку записи (для отслеживания активности станции, когда нет данных для реальной передачи)
+        mailer.setData(null, "ping.write", box);
+
+        //
+        if (count > 0) {
+            log.info("sendMail done, wsId: " + wsId + ", box: " + box + ", send: " + sendTask.sendFrom + " .. " + sendTask.sendTo + ", done count: " + count);
+        } else {
+            log.info("sendMail done, wsId: " + wsId + ", box: " + box + ", send: " + sendTask.sendFrom + ", nothing done");
+        }
+    }
+
+    /**
+     * Выясняем объем передачи
+     */
+    private static UtMailSendTask getSendTask(IJdxQue que, IMailer mailer, String box, IJdxMailStateManager mailStateManager) throws Exception {
         // Выясняем объем передачи: узнаем, сколько просит станция
         SendRequiredInfo requiredInfo = mailer.getSendRequired(box);
 
         // Узнаем, сколько есть у нас
-        long queMaxNo = que.getMaxNo();
+        long queNo = que.getMaxNo();
         // Узнаем, какая последняя отправленная
-        long queLastSendNo = mailStateManager.getMailSendDone();
+        long mailLastSendNo = mailStateManager.getMailSendDone();
 
         //
         long sendFrom;
@@ -40,13 +83,13 @@ public class UtMail {
                 log.warn("Repeat send required, from: " + requiredInfo.requiredFrom + ", to: " + requiredInfo.requiredTo + ", recreate: " + requiredInfo.recreate);
             } else {
                 // Зададим сами (до последней, что у нас есть на раздачу)
-                sendTo = queMaxNo;
+                sendTo = queNo;
                 log.warn("Repeat send required, from: " + requiredInfo.requiredFrom + ", recreate: " + requiredInfo.recreate);
             }
         } else {
             // Не просили - зададим сами (от последней отправленной до последней, что у нас есть на раздачу)
-            sendFrom = queLastSendNo + 1;
-            sendTo = queMaxNo;
+            sendFrom = mailLastSendNo + 1;
+            sendTo = queNo;
 
             // Проверки: не отправляли ли ранее такую реплику?
             // Защита от ситуации "восстановление БД из бэкапа", а также
@@ -57,8 +100,7 @@ public class UtMail {
                 throw new XError("invalid replica.no, send.no: " + sendFrom + ", srv.no: " + srv_no + ", server is forward");
             } else if (sendFrom == srv_no && srv_no != 0 && requiredInfo.requiredFrom == -1) {
                 // Рабочая станция одинакова с сервером
-                //IReplicaInfo fileInfo = getLastReplicaInfo(box);
-                IReplicaInfo fileInfo =((MailerHttp) mailer).getLastReplicaInfo(box);
+                IReplicaInfo fileInfo = ((MailerHttp) mailer).getLastReplicaInfo(box);
                 // Если последнее письмо совпадает - то считаем это недоразумением ит игнорируем.
                 IReplica replica = que.get(sendFrom);
                 if (!UtJdx.equalReplicaCrc(replica, fileInfo.getCrc())) {
@@ -73,44 +115,14 @@ public class UtMail {
         }
 
         //
-        long count = 0;
-        for (long no = sendFrom; no <= sendTo; no++) {
-            IReplica replica = que.get(no);
-
-            // Читаем заголовок
-            JdxReplicaReaderXml.readReplicaInfo(replica);
-
-            // Физически отправим реплику
-            mailer.send(replica, box, no);
-
-            // Отметим отправку очередного номера реплики (если отметка двигается вперед)
-            if (no > queLastSendNo) {
-                mailStateManager.setMailSendDone(no);
-            }
-
-            //
-            count = count + 1;
-        }
-
-
-        // Снимем флаг просьбы станции
-        if (requiredInfo.requiredFrom != -1) {
-            mailer.setSendRequired(box, new SendRequiredInfo());
-            log.warn("Repeat send done");
-        }
-
-
-        // Отметим попытку записи (для отслеживания активности станции, когда нет данных для реальной передачи)
-        mailer.setData(null, "ping.write", box);
-
+        UtMailSendTask sendTask = new UtMailSendTask();
+        sendTask.sendFrom = sendFrom;
+        sendTask.sendTo = sendTo;
+        sendTask.required = requiredInfo.requiredFrom != -1;
+        sendTask.mailLastSendNo = mailLastSendNo;
 
         //
-        if (count > 0) {
-            log.info("sendMail done, wsId: " + wsId + ", box: " + box + ", send: " + sendFrom + " .. " + sendTo + ", done count: " + count);
-        } else {
-            log.info("sendMail done, wsId: " + wsId + ", box: " + box + ", send: " + sendFrom + ", nothing done");
-        }
-
+        return sendTask;
     }
 
 
---
+++ jdtx-repl-main/src/jdtx/repl/main/api/util/UtMailSendTask.java
@@ -0,0 +1,10 @@
+package jdtx.repl.main.api.util;
+
+public class UtMailSendTask {
+
+    long sendFrom;
+    long sendTo;
+    boolean required;
+    long mailLastSendNo;
+
+}
