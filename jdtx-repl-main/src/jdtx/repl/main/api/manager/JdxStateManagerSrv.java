710b5dd1efc020e2db4502e01e97b4292f0713bc
sazonov.den <sazonov.den@gmail.com>
2022-09-08 02:23:36 +0600

    -

--- jdtx-repl-main/src/jdtx/repl/main/api/manager/JdxStateManagerSrv.java
+++ jdtx-repl-main/src/jdtx/repl/main/api/manager/JdxStateManagerSrv.java
@@ -1,9 +1,6 @@
 package jdtx.repl.main.api.manager;
 
-import jandcode.dbm.data.*;
 import jandcode.dbm.db.*;
-import jandcode.utils.error.*;
-import jdtx.repl.main.api.util.*;
 
 /**
  * Состояние серверных задач: отметки в БД, насколько отработаны очереди и прочее.
