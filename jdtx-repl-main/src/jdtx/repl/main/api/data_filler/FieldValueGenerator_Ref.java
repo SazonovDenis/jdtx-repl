7f1221ea60763b079635f2ebcb38c7174db18add
sazonov.den <sazonov.den@gmail.com>
2022-09-13 00:01:47 +0600

    Рефакторинг FieldValueGenerator_Ref, прочий рефакторинг

---
+++ jdtx-repl-main/src/jdtx/repl/main/api/data_filler/FieldValueGenerator_Ref.java
@@ -0,0 +1,40 @@
+package jdtx.repl.main.api.data_filler;
+
+import jandcode.dbm.db.*;
+import jdtx.repl.main.api.struct.*;
+
+import java.util.*;
+
+class FieldValueGenerator_Ref extends FieldValueGenerator {
+
+    Db db;
+    IJdxDbStruct struct;
+    IDataFiller filler;
+    UtFiller utFiller;
+
+    public FieldValueGenerator_Ref(Db db, IJdxDbStruct struct, IDataFiller filler) {
+        this.db = db;
+        this.struct = struct;
+        this.filler = filler;
+        this.utFiller = new UtFiller(db, struct);
+    }
+
+
+    @Override
+    public Object genValue(IJdxField field) throws Exception {
+        String keyField = filler.getFieldKey(field);
+        Set<Long> refValues = filler.getRefValuesCache().get(keyField);
+
+        //
+        if (refValues == null) {
+            // Правильно подготовим набор значений для ссылочных полей
+            refValues = utFiller.loadAllIds(field.getRefTable());
+            // Закэшируем набор значений - повторно ссылки искать - дорого
+            filler.getRefValuesCache().put(keyField, refValues);
+        }
+
+        //
+        return utFiller.selectOneObject(refValues);
+    }
+
+}
